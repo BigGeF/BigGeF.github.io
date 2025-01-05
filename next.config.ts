5d1b3e729a97c3d5ae0f47923fa6402dc3164985
BigGeF <52731933+BigGeF@users.noreply.github.com>
2025-01-05 06:57:18 -0800

    removed all and add single page

--- next.config.ts
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  output: 'export', // 将项目导出为静态文件
-  basePath: '/BigGeF', // 将 `<repository-name>` 替换为 GitHub 仓库名称
-  assetPrefix: '/BigGeF',
-};
